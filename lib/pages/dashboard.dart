fe40280cb57827a7da818b6f7d37965ff6e0115a
adeveloper-wq <dajesch@gmail.com>
2021-09-10 10:09:27 +0200

    fetch groups on dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rezeptverwaltung/domains/user.dart';
 import 'package:rezeptverwaltung/providers/auth_refresh_provider.dart';
+import 'package:rezeptverwaltung/providers/group_provider.dart';
 import 'package:rezeptverwaltung/providers/user_provider.dart';
 import 'package:rezeptverwaltung/util/user_preferences.dart';
 
@@ -12,14 +13,44 @@ class DashBoard extends StatefulWidget {
 
 class _DashBoardState extends State<DashBoard> {
   User user;
+  List<dynamic> _userGroups;
+
+  int _activeGroupId;
+
+  void getGroups() {
+    final Future<Map<String, dynamic>> successfulMessage =
+        context.read<GroupProvider>().getGroups();
+
+    successfulMessage.then((response) {
+      if (response['status']) {
+        setState(() {
+          _userGroups = response['data'];
+        });
+        setState(() {
+          _activeGroupId = _userGroups[0]['G_ID'];
+        });
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(response['message'])));
+      }
+    });
+  }
+
   @override
   void initState() {
-    user = context.read<UserProvider>().user;
     super.initState();
+    user = context.read<UserProvider>().user;
+    this.getGroups();
   }
 
   @override
   Widget build(BuildContext context) {
+    var setActiveGroup = (int id) {
+      setState(() {
+        _activeGroupId = id;
+      });
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: Text("DASHBOARD PAGE"),
@@ -38,7 +69,31 @@ class _DashBoardState extends State<DashBoard> {
       ),
       body: Column(
         children: [
-          SizedBox(height: 100,),
+          if (_userGroups != null)
+            // Handle your callback
+            Container(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  for (var item in _userGroups)
+                    InkWell(
+                        onTap: () {
+                          setActiveGroup(item['G_ID']);
+                        },
+                        child: Text(
+                          item['name'],
+                          style: TextStyle(
+                              backgroundColor: _activeGroupId == item['G_ID']
+                                  ? Colors.grey
+                                  : Colors.transparent),
+                        )),
+                ],
+              ),
+            ),
+          SizedBox(
+            height: 20,
+          ),
           Center(child: Text(user != null ? user.email : '')),
         ],
       ),
